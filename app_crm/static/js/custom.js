39093a37da7c56903548fe6d36ad8692f7499c58
Viktor Radchenko <viktor.radchenko@playtech.com>
2022-07-04 15:59:20 +0400

    UI-2: feedback added

--- app_crm/static/js/custom.js
+++ app_crm/static/js/custom.js
@@ -25,8 +25,21 @@ function processNotifications() {
 
 processNotifications();
 
-// $("#accordionSidebar").hover(function() {
-//   setTimeout(function() {
-//     $("#accordionSidebar").toggleClass("toggled")
-//   }, 2000);
-// })
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("feedback__btn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
