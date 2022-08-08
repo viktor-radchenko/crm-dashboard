9d4e2397ee3d7af57fe8458a206f2179ccca56bc
Viktor Radchenko <viktor.radchenko@playtech.com>
2022-08-09 00:06:57 +0400

    UI-2: fixed ui issues with reordering

--- app_crm/static/js/createform.js
+++ app_crm/static/js/createform.js
@@ -8,27 +8,27 @@ $(document).ready(function() {
 
     $(add_button).click(function(e) {
         e.preventDefault();
-        $(wrapper).append('<div class="form-group col-12 m-2 p-2 card mb-4 shadow tosendgroup"><label class="text ml-1">Title:</label><input type="text" class="form-control mb-2" name="textfield" required><a href="#" class="delete">Delete</a></div>');
+        $(wrapper).append('<div class="form-group col-12 p-2 card mb-4 shadow tosendgroup"><label class="text ml-1">Title:</label><div class="drgme" style="display:inline-block; cursor: pointer; position: absolute; right: 20px;"><i class="fas fa-bars"></i></div><input type="text" class="form-control mb-2" name="textfield" required><a href="#" class="delete">Delete</a></div>');
     });
 
     $(add_checkbox_button).click(function(e) {
         e.preventDefault();
-        $(wrapper).append('<div class="form-group col-12 m-2 p-2 mb-4 card shadow tosendgroup"><label class="text ml-1">Checkbox field. Title:</label><input type="text" class="form-control mb-2" name="checkboxname" required><div class="checkboxes"><label class="text ml-1">Checkbox:</label><br><a href="#" class="addcheckbox">Add checkbox</a></div><a href="#" class="delete">Delete group</a></div>');
+        $(wrapper).append('<div class="form-group col-12 p-2 mb-4 card shadow tosendgroup"><label class="text ml-1">Checkbox field. Title:</label><div class="drgme" style="display: inline-block; cursor: pointer; position: absolute; right: 20px;"><i class="fas fa-bars"></i></div><input type="text" class="form-control mb-2" name="checkboxname" required><div class="checkboxes"><label class="text ml-1">Checkbox:</label><br><a href="#" class="addcheckbox">Add checkbox</a></div><a href="#" class="delete">Delete group</a></div>');
     });
 
     $(add_dropdown_button).click(function(e) {
         e.preventDefault();
-        $(wrapper).append('<div class="form-group col-12 m-2 p-2 card mb-4 shadow tosendgroup"><label class="text ml-1">Dropdown field. Title:</label><input type="text" class="form-control mb-2" name="dropdownname" required><div class="dropdown_items"><label class="text ml-1">Dropdow item:</label><br><a href="#" class="adddropdownitem">Add dropdown item</a></div><a href="#" class="delete">Delete group</a></div>');
+        $(wrapper).append('<div class="form-group col-12 p-2 card mb-4 shadow tosendgroup"><label class="text ml-1">Dropdown field. Title:</label><div class="drgme" style="display: inline-block; cursor: pointer; position: absolute; right: 20px;"><i class="fas fa-bars"></i></div><input type="text" class="form-control mb-2" name="dropdownname" required><div class="dropdown_items"><label class="text ml-1">Dropdow item:</label><br><a href="#" class="adddropdownitem">Add dropdown item</a></div><a href="#" class="delete">Delete group</a></div>');
     });
 
     $(add_radio_button).click(function(e) {
         e.preventDefault();
-        $(wrapper).append('<div class="form-group col-12 m-2 p-2 mb-4 card shadow tosendgroup"><label class="text ml-1">Radio field. Title:</label><input type="text" class="form-control mb-2" name="radioname" required><div class="radios"><label class="text ml-1">Radio:</label><br><a href="#" class="addradio">Add radio</a></div><a href="#" class="delete">Delete group</a></div>');
+        $(wrapper).append('<div class="form-group col-12 p-2 mb-4 card shadow tosendgroup"><label class="text ml-1">Radio field. Title:</label><div class="drgme" style="display: inline-block; cursor: pointer; position: absolute; right: 20px;"><i class="fas fa-bars"></i></div><input type="text" class="form-control mb-2" name="radioname" required><div class="radios"><label class="text ml-1">Radio:</label><br><a href="#" class="addradio">Add radio</a></div><a href="#" class="delete">Delete group</a></div>');
     });
 
     $(add_textarea_button).click(function(e) {
         e.preventDefault();
-        $(wrapper).append('<div class="form-group col-12 m-2 p-2 card mb-4 shadow tosendgroup"><label class="text ml-1">Text area title:</label><input type="text" class="form-control mb-2" name="textarea" required><a href="#" class="delete">Delete</a></div>');
+        $(wrapper).append('<div class="form-group col-12 p-2 card mb-4 shadow tosendgroup"><label class="text ml-1">Text area title:</label><div class="drgme" style="display: inline-block; cursor: pointer; position: absolute; right: 20px;"><i class="fas fa-bars"></i></div><input type="text" class="form-control mb-2" name="textarea" required><a href="#" class="delete">Delete</a></div>');
     });
 
     $(wrapper).on("click", ".delete", function(e) {
@@ -38,17 +38,17 @@ $(document).ready(function() {
 
     $(wrapper).on("click", ".addcheckbox", function(e) {
         e.preventDefault();
-        $(this).parent('div').append('<div class="checkbox m-2 p-2 card"><input type="text" class="form-control mb-2 col-10" name="checkboxitem" required><a href="#" class="delete">Delete checkbox</a></div>');
+        $(this).parent('div').append('<div class="checkbox p-2 card"><input type="text" class="form-control mb-2 col-10" name="checkboxitem" required><a href="#" class="delete">Delete checkbox</a></div>');
     });
 
     $(wrapper).on("click", ".addradio", function(e) {
         e.preventDefault();
-        $(this).parent('div').append('<div class="radio m-2 p-2 card"><input type="text" class="form-control mb-2 col-10" name="radioitem" required><a href="#" class="delete">Delete radio</a></div>');
+        $(this).parent('div').append('<div class="radio p-2 card"><input type="text" class="form-control mb-2 col-10" name="radioitem" required><a href="#" class="delete">Delete radio</a></div>');
     });
 
     $(wrapper).on("click", ".adddropdownitem", function(e) {
         e.preventDefault();
-        $(this).parent('div').append('<div class="dropdown_item m-2 p-2 card"><input type="text" class="form-control mb-2 col-10" name="dropdownitem" required><a href="#" class="delete">Delete dropdown item</a></div>');
+        $(this).parent('div').append('<div class="dropdown_item p-2 card"><input type="text" class="form-control mb-2 col-10" name="dropdownitem" required><a href="#" class="delete">Delete dropdown item</a></div>');
     });
 });
 
